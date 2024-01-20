91a668f6c5f95b3c5d86feed242352809c5ac6c4
India Jackson <ijackson1@gsu.edu>
2024-01-20 14:10:41 -0500

    Update creating_pyhc_kernel.sh

--- creating_pyhc_kernel.sh
+++ creating_pyhc_kernel.sh
@@ -10,7 +10,8 @@ conda activate pyhc-all
 # Step 3: Add requirements.txt
 pip install --no-cache-dir numpy==1.24.3
 pip install --no-cache-dir spacepy --no-build-isolation
-pip install -r ~/shared/python_libraries_dependencies/requirements.txt
+# pip install -r ~/shared/python_libraries_dependencies/requirements.txt
+pip install --use-pep517 --retries 5 --no-cache-dir -r ~/shared/python_libraries_dependencies/requirements.txt
 pip install --no-cache-dir git+https://github.com/nasa/Kamodo.git
 pip install --no-cache-dir pytplot==1.7.28
 pip install --no-cache-dir pytplot-mpl-temp
