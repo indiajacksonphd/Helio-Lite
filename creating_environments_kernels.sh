99af70137998f6742a05b6a772d136ed184451fb
India Jackson <ijackson1@gsu.edu>
2024-01-20 12:30:05 -0500

    Create creating_environments_kernels.sh

---
+++ creating_environments_kernels.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Step 1: Add environment.yaml
+conda env create -f ~/shared/python_libraries_dependencies/environment.yml
+
+# Step 2: Activate the environment
+conda activate pyhc-all
+
+# Step 3: Add requirements.txt
+pip install --no-cache-dir numpy==1.24.3
+pip install --no-cache-dir spacepy --no-build-isolation
+pip3 install -r ~/shared/python_libraries_dependencies/requirements.txt
+pip install --no-cache-dir git+https://github.com/nasa/Kamodo.git
+pip install --no-cache-dir pytplot==1.7.28
+pip install --no-cache-dir pytplot-mpl-temp
+pip install --no-cache-dir pyspedas
+
+# Step 4: Add environment.yml to shared library directory
+conda env export > ~/shared/newly_created_python_envs/pyhcAll.yml
+
+# Step 5: Create the environment in the shared JupyterHub environment
+sudo conda env create --prefix /opt/tljh/user/envs/pyhc-all --file ~/shared/newly_created_python_envs/pyhcAll.yml
+
+# Step 6: Make it into a kernel
+conda install ipykernel -y
+ipython kernel install --user --name=pyhc-all --display-name "PyHC All Packages"
+
+# Step 7: Clean up and restart JupyterHub
+conda deactivate
+sudo systemctl restart jupyterhub.service
