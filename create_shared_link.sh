ed5ebfcb3cb46e436d62af6c566293fbd0e2a84b
India Jackson <ijackson1@gsu.edu>
2024-01-20 11:02:41 -0500

    Update create_shared_link.sh

--- create_shared_link.sh
+++ create_shared_link.sh
@@ -9,14 +9,3 @@ sudo chmod +x /etc/profile.d/create_shared_link.sh
 sudo chmod +x /etc/profile.d/create_shared_link.sh
 
 sudo chmod -R 777 /shared
-
-But will this script execute everytime the user or admin login?
-
-# Download the script using curl
-sudo curl -o /etc/profile.d/create_shared_link.sh https://raw.githubusercontent.com/username/repository/branch/create_shared_link.sh
-
-# Make the script executable
-sudo chmod +x /etc/profile.d/create_shared_link.sh
-
-# Set the appropriate permissions on /shared
-sudo chmod -R 775 /shared
