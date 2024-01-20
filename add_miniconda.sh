d39477d30fff981fb9f5c801dec9e3ba7c0d469b
India Jackson <ijackson1@gsu.edu>
2024-01-20 11:18:38 -0500

    Update add_miniconda.sh

--- add_miniconda.sh
+++ add_miniconda.sh
@@ -3,3 +3,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
 rm Miniconda3-latest-Linux-x86_64.sh
 sudo systemctl restart jupyterhub.service
+
+sudo curl -o /etc/profile.d/miniconda.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/share_miniconda.sh
