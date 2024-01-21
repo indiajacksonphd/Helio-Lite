c8f7c57ef0e26619c3b8feacfdd1c200f742e331
India Jackson <ijackson1@gsu.edu>
2024-01-21 11:22:54 -0500

    Create creating_pyhc_kernel_2.sh

---
+++ creating_pyhc_kernel_2.sh
@@ -0,0 +1,23 @@
+# Step 1: create new environment in shared miniconda
+sudo conda env create --prefix /opt/miniconda/envs/pyhc-all -f /shared/python_libraries_dependencies/environment.yml
+
+# Step 2: Activate the environment
+sudo conda activate /opt/miniconda/envs/pyhc-all
+
+# Step 3: Fill it up with all packages
+sudo apt-get install -y gcc g++ gfortran ncurses-dev build-essential cmake
+
+sudo pip install --no-cache-dir numpy==1.24.3
+sudo pip install --no-cache-dir spacepy --no-build-isolation
+sudo pip install --use-pep517 --retries 5 --no-cache-dir -r /shared/python_libraries_dependencies/requirements.txt
+sudo pip install --no-cache-dir pytplot==1.7.28
+sudo pip install --no-cache-dir pytplot-mpl-temp
+sudo pip install --no-cache-dir pyspedas
+
+# Step 4: Make it into a kernel, save in tljh
+sudo conda install ipykernel -y
+sudo ipython kernel install --prefix /opt/tljh/user/ --name=pyhc-all --display-name "PyHC All Packages"
+
+# Step 7: Clean up and restart JupyterHub
+conda deactivate
+sudo systemctl restart jupyterhub.service
