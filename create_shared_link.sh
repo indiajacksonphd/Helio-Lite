ea60ff5b56a1f89abc89b7b71218e395d653022d
India Jackson <ijackson1@gsu.edu>
2024-01-21 12:14:57 -0500

    Update create_shared_link.sh

--- create_shared_link.sh
+++ create_shared_link.sh
@@ -4,8 +4,4 @@ if [ ! -L "$HOME/shared" ] && [ -d "/shared" ]; then
     ln -s /shared "$HOME/shared"
 fi
 
-sudo chmod +x /etc/profile.d/create_shared_link.sh
-
-sudo chmod +x /etc/profile.d/create_shared_link.sh
-
 sudo chmod -R 777 /shared
