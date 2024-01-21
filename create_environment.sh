9e581182005e781272e24c88e1af8ff8b175a2c0
India Jackson <ijackson1@gsu.edu>
2024-01-21 17:09:00 -0500

    Update create_environment.sh

--- create_environment.sh
+++ create_environment.sh
@@ -3,16 +3,19 @@
 # Pull in shell scripts
 curl -o start_up.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/start_up.sh
 curl -o add_miniconda.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/add_miniconda.sh
+curl -o create_aiml_kernel.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/create_aiml_kernel.sh
 curl -o creating_pyhc_kernel_2.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/creating_pyhc_kernel_2.sh
 
 # Make shell scripts executable
 chmod +x start_up.sh
 chmod +x add_miniconda.sh
+chmod +x create_aiml_kernel.sh
 chmod +x creating_pyhc_kernel_2.sh
 
 # Run shell scripts
 ./start_up.sh
 ./add_miniconda.sh
+./create_aiml_kernel.sh
 ./creating_pyhc_kernel_2.sh
 
 sudo chmod -R 777 /shared
