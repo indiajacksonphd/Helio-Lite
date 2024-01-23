a7ce72fa975a8440cba1cc9e1a8a225570bd1d3a
India Jackson <ijackson1@gsu.edu>
2024-01-23 11:34:04 -0500

    Update create_aiml_kernel.sh

--- kernel_creation/create_aiml_kernel.sh
+++ kernel_creation/create_aiml_kernel.sh
@@ -1,13 +1,13 @@
 # Step 0: Initialize Conda (if not already done)
 eval "$(conda shell.bash hook)"
 
-# Step 1: create new environment in shared jupyterhub environment
+# Step 1: create new environment in shared JupyterHub so that all admin and users can access it
 sudo conda env create --prefix /opt/tljh/user/envs/ai-ml -f /shared/python_libraries_dependencies/ml.yml
 
 # Step 2: Activate the environment
 sudo conda activate /opt/tljh/user/envs/ai-ml
 
-# Step 3: Make it into a kernel, save in tljh
+# Step 3: Make it into a kernel, save in JupyterHub so that all admin and users can access it
 sudo conda install ipykernel -y
 sudo ipython kernel install --prefix /opt/tljh/user/ --name=ai-ml --display-name "AI-ML Packages"
 
