6b4cecd7add10d5452dcf1986747735695086057
India Jackson <ijackson1@gsu.edu>
2024-01-23 11:45:43 -0500

    Create create_aiml_kernel.sh

---
+++ old_scripts/create_aiml_kernel.sh
@@ -0,0 +1,16 @@
+# Step 0: Initialize Conda (if not already done)
+eval "$(conda shell.bash hook)"
+
+# Step 1: create new environment in shared miniconda
+sudo conda env create --prefix /opt/miniconda/envs/ai-ml -f /shared/python_libraries_dependencies/ml.yml
+
+# Step 2: Activate the environment
+sudo conda activate /opt/miniconda/envs/ai-ml
+
+# Step 3: Make it into a kernel, save in tljh
+sudo conda install ipykernel -y
+sudo ipython kernel install --prefix /opt/tljh/user/ --name=ai-ml --display-name "AI-ML Packages"
+
+# Step 7: Clean up and restart JupyterHub
+conda deactivate
+sudo systemctl restart jupyterhub.service
