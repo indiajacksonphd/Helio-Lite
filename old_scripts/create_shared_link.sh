c74b7e7ef4084a0d77c5c41a0070e0fd96e98034
India Jackson <ijackson1@gsu.edu>
2024-01-23 18:54:47 -0500

    Delete old_scripts directory

--- old_scripts/create_shared_link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -L "$HOME/shared" ] && [ -d "/shared" ]; then
-    ln -s /shared "$HOME/shared"
-fi
-
-sudo chmod -R 777 /shared
--- old_scripts/share_miniconda.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="/opt/miniconda/bin:$PATH"
-sudo chmod -R ugo+rx /opt/miniconda
-conda init bash
