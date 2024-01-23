26b7689dbfcff1c33f3059902e05f8d64fe97025
India Jackson <ijackson1@gsu.edu>
2024-01-23 11:40:25 -0500

    Delete add_miniconda.sh

--- add_miniconda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
-rm Miniconda3-latest-Linux-x86_64.sh
-sudo systemctl restart jupyterhub.service
-
-sudo curl -o /etc/profile.d/miniconda.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/share_miniconda.sh
