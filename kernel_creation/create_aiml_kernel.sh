b82ea5a4ee6983b00171092bc11ea38e3b2699cd
India Jackson <ijackson1@gsu.edu>
2024-01-29 16:11:54 -0500

    Update create_aiml_kernel.sh

--- kernel_creation/create_aiml_kernel.sh
+++ kernel_creation/create_aiml_kernel.sh
@@ -7,6 +7,11 @@ sudo conda env create --prefix /opt/tljh/user/envs/ai-ml -f /shared/python_libra
 # Step 2: Activate the environment
 sudo conda activate /opt/tljh/user/envs/ai-ml
 
+# Step 4: Copy your Python modules to the environment directory
+cp /shared/custom_modules/aiaImage.py /opt/tljh/user/envs/ai-ml
+cp /shared/custom_modules/donkiData.py /opt/tljh/user/envs/ai-ml
+cp /shared/custom_modules/dmLab.py /opt/tljh/user/envs/ai-ml
+
 # Step 3: Make it into a kernel, save in JupyterHub so that all admin and users can access it
 sudo conda install ipykernel -y
 sudo ipython kernel install --prefix /opt/tljh/user/ --name=ai-ml --display-name "AI-ML Packages"
