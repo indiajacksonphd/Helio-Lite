b3aaf204d71216c74231669589df6f2d544b0b1f
India Jackson <ijackson1@gsu.edu>
2024-01-29 16:10:02 -0500

    Update create_pyhc_kernel.sh

--- kernel_creation/create_pyhc_kernel.sh
+++ kernel_creation/create_pyhc_kernel.sh
@@ -13,9 +13,15 @@ sudo apt-get install -y gcc g++ gfortran ncurses-dev build-essential cmake
 sudo pip install --no-cache-dir numpy==1.24.3
 sudo pip install --no-cache-dir spacepy --no-build-isolation
 sudo pip install --use-pep517 --retries 5 --no-cache-dir -r /shared/python_libraries_dependencies/requirements.txt
+sudo pip install --use-pep517 --retries 5 --no-cache-dir -r /shared/python_libraries_dependencies/custom_requirements.txt
 sudo pip install --no-cache-dir pytplot==1.7.28
 sudo pip install --no-cache-dir pytplot-mpl-temp
 sudo pip install --no-cache-dir pyspedas
+
+# Step 4: Copy your Python modules to the environment directory
+cp aiaImage.py /opt/tljh/user/envs/pyhc-all
+cp donkiData.py /opt/tljh/user/envs/pyhc-all
+cp dmLab.py /opt/tljh/user/envs/pyhc-all
 
 # Step 4: Make it into a kernel, save in tljh
 sudo conda install ipykernel -y
