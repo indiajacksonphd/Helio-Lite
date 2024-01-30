30cbb87b4f380be613219102cbafe8902386595a
India Jackson <ijackson1@gsu.edu>
2024-01-29 19:14:46 -0500

    Update 1_create_directories.sh

--- START_HERE/1_create_directories.sh
+++ START_HERE/1_create_directories.sh
@@ -3,40 +3,40 @@
 # Step 1: Grab CDF Library and move the directory to /usr/lib/
 sudo git clone https://github.com/heliophysicsPy/pyhc-docker-environment.git /usr/lib/pyhc-docker-environment && sudo mv /usr/lib/pyhc-docker-environment/docker/pyhc-environment/contents/cdf38_0-dist /usr/lib/
 
-sudo mkdir /shared
-sudo mkdir /shared/examples/PyHC
-sudo mkdir /shared/examples/AI_ML
-sudo mkdir /shared/newly_created_python_envs
-sudo mkdir /shared/python_libraries_dependencies
-sudo mkdir /shared/custom_modules
+sudo mkdir ~/shared
+sudo mkdir ~/shared/examples/PyHC
+sudo mkdir ~/shared/examples/AI_ML
+sudo mkdir ~/shared/newly_created_python_envs
+sudo mkdir ~/shared/python_libraries_dependencies
+sudo mkdir ~/shared/custom_modules
 
-sudo chmod -R 777 /shared
+# sudo chmod -R 777 /shared
 
-sudo mkdir -p /shared/.sunpy
-sudo mkdir -p /shared/heliopy/data
-sudo mkdir -p /shared/Geospacelab/Data
-sudo mkdir -p /shared/.spacepy/data
+sudo mkdir -p ~/shared/.sunpy
+sudo mkdir -p ~/shared/heliopy/data
+sudo mkdir -p ~/shared/Geospacelab/Data
+sudo mkdir -p ~/shared/.spacepy/data
 
 #PyHC examples
-curl -o /shared/examples/PyHC/coordinate_systems.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/coordinate_systems.ipynb
-curl -o /shared/examples/PyHC/coordinates_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/coordinates_demo.ipynb
-curl -o /shared/examples/PyHC/import-test.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/import-test.ipynb
-curl -o /shared/examples/PyHC/planet_locations.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/planet_locations.ipynb
-curl -o /shared/examples/PyHC/pytplot_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/pytplot_demo.ipynb
-curl -o /shared/examples/PyHC/retrieve_compress.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/retrieve_compress.ipynb
-curl -o /shared/examples/PyHC/units_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/units_demo.ipynb
-curl -o /shared/examples/PyHC/Combining_HPCA_and_EIS_energy_spectra.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/Combining_HPCA_and_EIS_energy_spectra.ipynb
+curl -o ~/shared/examples/PyHC/coordinate_systems.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/coordinate_systems.ipynb
+curl -o ~/shared/examples/PyHC/coordinates_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/coordinates_demo.ipynb
+curl -o ~/shared/examples/PyHC/import-test.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/import-test.ipynb
+curl -o ~/shared/examples/PyHC/planet_locations.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/planet_locations.ipynb
+curl -o ~/shared/examples/PyHC/pytplot_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/pytplot_demo.ipynb
+curl -o ~/shared/examples/PyHC/retrieve_compress.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/retrieve_compress.ipynb
+curl -o ~/shared/examples/PyHC/units_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/units_demo.ipynb
+curl -o ~/shared/examples/PyHC/Combining_HPCA_and_EIS_energy_spectra.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/Combining_HPCA_and_EIS_energy_spectra.ipynb
 
 # AI_ML Examples
-curl -o /shared/examples/AI_ML/04.14-Visualization-With-Seaborn.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/04.14-Visualization-With-Seaborn.ipynb
+curl -o ~/shared/examples/AI_ML/04.14-Visualization-With-Seaborn.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/04.14-Visualization-With-Seaborn.ipynb
 
-curl -o /shared/python_libraries_dependencies/environment.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/environment.yml
-curl -o /shared/python_libraries_dependencies/requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/requirements.txt
-curl -o /shared/python_libraries_dependencies/ml.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/ml.yml
-curl -o /shared/python_libraries_dependencies/custom_requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/custom_requirements.txt
-curl -o /shared/custom_modules/aiaImages.py https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/custom_requirements.txt
+# SDO DONKI and DMLab Examples
+
+curl -o ~/shared/python_libraries_dependencies/environment.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/environment.yml
+curl -o ~/shared/python_libraries_dependencies/requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/requirements.txt
+curl -o ~/shared/python_libraries_dependencies/ml.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/ml.yml
+curl -o ~/shared/python_libraries_dependencies/custom_requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/custom_requirements.txt
 
 
-
-sudo curl -o /etc/profile.d/link_directories.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/START_HERE/link_directories.sh
-sudo chmod +x /etc/profile.d/link_directories.sh
+# sudo curl -o /etc/profile.d/link_directories.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/START_HERE/link_directories.sh
+# sudo chmod +x /etc/profile.d/link_directories.sh
