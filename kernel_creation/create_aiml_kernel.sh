46560f880432a3ef2b6ae3b29a29a146027dabb5
India Jackson <ijackson1@gsu.edu>
2024-01-29 19:43:19 -0500

    Update create_aiml_kernel.sh

--- kernel_creation/create_aiml_kernel.sh
+++ kernel_creation/create_aiml_kernel.sh
@@ -12,6 +12,8 @@ cp ~/shared/custom_modules/aiaImage.py /opt/tljh/user/envs/ai-ml
 cp ~/shared/custom_modules/donkiData.py /opt/tljh/user/envs/ai-ml
 cp ~/shared/custom_modules/dmLab.py /opt/tljh/user/envs/ai-ml
 
+sudo pip install --use-pep517 --retries 5 --no-cache-dir -r /shared/python_libraries_dependencies/custom_requirements.txt
+
 # Step 3: Make it into a kernel, save in JupyterHub so that all admin and users can access it
 sudo conda install ipykernel -y
 sudo ipython kernel install --prefix /opt/tljh/user/ --name=ai-ml --display-name "AI-ML Packages"
