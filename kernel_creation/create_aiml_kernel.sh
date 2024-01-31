2bb112f214b1d7062ea7826b5af891d65fae3a76
India Jackson <ijackson1@gsu.edu>
2024-01-30 21:43:54 -0500

    Update create_aiml_kernel.sh

--- kernel_creation/create_aiml_kernel.sh
+++ kernel_creation/create_aiml_kernel.sh
@@ -2,7 +2,7 @@
 eval "$(conda shell.bash hook)"
 
 # Step 1: create new environment in shared JupyterHub so that all admin and users can access it
-sudo conda env create --prefix /opt/tljh/user/envs/ai-ml -f /shared/python_libraries_dependencies/ml.yml
+sudo conda env create --prefix /opt/tljh/user/envs/ai-ml -f /python_libraries_dependencies/ml.yml
 
 # Step 2: Activate the environment
 sudo conda activate /opt/tljh/user/envs/ai-ml
@@ -12,7 +12,7 @@ cp ~/shared/custom_modules/aiaImage.py /opt/tljh/user/envs/ai-ml
 cp ~/shared/custom_modules/donkiData.py /opt/tljh/user/envs/ai-ml
 cp ~/shared/custom_modules/dmLab.py /opt/tljh/user/envs/ai-ml
 
-sudo pip install --use-pep517 --retries 5 --no-cache-dir -r /shared/python_libraries_dependencies/custom_requirements.txt
+sudo pip install --use-pep517 --retries 5 --no-cache-dir -r /python_libraries_dependencies/custom_requirements.txt
 
 # Step 3: Make it into a kernel, save in JupyterHub so that all admin and users can access it
 sudo conda install ipykernel -y
