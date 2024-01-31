6b68a3a86ae54e798b976bee45f355132c12e126
India Jackson <ijackson1@gsu.edu>
2024-01-30 21:45:17 -0500

    Update link_directories.sh

--- START_HERE/link_directories.sh
+++ START_HERE/link_directories.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -L "$HOME/shared" ] && [ -d "/shared" ]; then
-    ln -s /shared "$HOME/shared"
+if [ ! -L "$HOME/examples" ] && [ -d "/examples" ]; then
+    ln -s /examples "$HOME/examples"
 fi
 
-sudo chmod -R 777 /shared
+sudo chmod -R 777 /examples
