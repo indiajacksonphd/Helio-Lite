aae48ad47630e7e873949da396579c1432581723
India Jackson, PhD <ijackson1@gsu.edu>
2025-08-09 20:42:11 -0400

    Update create_directories.sh

--- START_HERE/create_directories.sh
+++ START_HERE/create_directories.sh
@@ -19,40 +19,40 @@ sudo mkdir -p /shared/.spacepy/data
 sudo chmod -R 777 /shared
 
 #PyHC examples
-curl -o /shared/examples/PyHC/coordinate_systems.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/coordinate_systems.ipynb
-curl -o /shared/examples/PyHC/coordinates_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/coordinates_demo.ipynb
-curl -o /shared/examples/PyHC/planet_locations.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/planet_locations.ipynb
-curl -o /shared/examples/PyHC/pytplot_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/pytplot_demo.ipynb
-curl -o /shared/examples/PyHC/units_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/PyHC/units_demo.ipynb
+curl -o /shared/examples/PyHC/coordinate_systems.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/PyHC/coordinate_systems.ipynb
+curl -o /shared/examples/PyHC/coordinates_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/PyHC/coordinates_demo.ipynb
+curl -o /shared/examples/PyHC/planet_locations.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/PyHC/planet_locations.ipynb
+curl -o /shared/examples/PyHC/pytplot_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/PyHC/pytplot_demo.ipynb
+curl -o /shared/examples/PyHC/units_demo.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/PyHC/units_demo.ipynb
 
 # AI_ML Examples
-curl -o /shared/examples/AI_ML/marathon-data.csv https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/marathon-data.csv
-curl -o /shared/examples/AI_ML/pima-indians-diabetes.data.csv https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/pima-indians-diabetes.data.csv
-curl -o /shared/examples/AI_ML/pytorch_torchvision_RNN_PT.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/pytorch_torchvision_RNN_PT.ipynb
-curl -o /shared/examples/AI_ML/seaborn.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/seaborn.ipynb
-curl -o /shared/examples/AI_ML/tensorflow_keras_classification.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/tensorflow_keras_classification.ipynb
-curl -o /shared/examples/AI_ML/xgboost.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AI_ML/xgboost.ipynb
+curl -o /shared/examples/AI_ML/marathon-data.csv https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AI_ML/marathon-data.csv
+curl -o /shared/examples/AI_ML/pima-indians-diabetes.data.csv https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AI_ML/pima-indians-diabetes.data.csv
+curl -o /shared/examples/AI_ML/pytorch_torchvision_RNN_PT.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AI_ML/pytorch_torchvision_RNN_PT.ipynb
+curl -o /shared/examples/AI_ML/seaborn.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AI_ML/seaborn.ipynb
+curl -o /shared/examples/AI_ML/tensorflow_keras_classification.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AI_ML/tensorflow_keras_classification.ipynb
+curl -o /shared/examples/AI_ML/xgboost.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AI_ML/xgboost.ipynb
 
 # SDO DONKI and DMLab Examples
-curl -o /shared/examples/AIA_HMI_DONKI_DMLab/AIA_DONKI.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AIA_DONKI_DMLab/AIA_DONKI.ipynb
-curl -o /shared/examples/AIA_HMI_DONKI_DMLab/HMI_DONKI.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AIA_DONKI_DMLab/HMI_DONKI.ipynb
-curl -o /shared/examples/AIA_HMI_DONKI_DMLab/DMLab.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AIA_DONKI_DMLab/DMLab.ipynb
-curl -o /shared/examples/AIA_HMI_DONKI_DMLab/GSEP_DMLab.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AIA_DONKI_DMLab/GSEP_DMLab.ipynb
-curl -o /shared/examples/AIA_HMI_DONKI_DMLab/TimeConversion.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AIA_DONKI_DMLab/TimeConversion.ipynb
-curl -o /shared/examples/AIA_HMI_DONKI_DMLab/TimeZones.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/examples/AIA_DONKI_DMLab/TimeZones.ipynb
+curl -o /shared/examples/AIA_HMI_DONKI_DMLab/AIA_DONKI.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AIA_DONKI_DMLab/AIA_DONKI.ipynb
+curl -o /shared/examples/AIA_HMI_DONKI_DMLab/HMI_DONKI.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AIA_DONKI_DMLab/HMI_DONKI.ipynb
+curl -o /shared/examples/AIA_HMI_DONKI_DMLab/DMLab.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AIA_DONKI_DMLab/DMLab.ipynb
+curl -o /shared/examples/AIA_HMI_DONKI_DMLab/GSEP_DMLab.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AIA_DONKI_DMLab/GSEP_DMLab.ipynb
+curl -o /shared/examples/AIA_HMI_DONKI_DMLab/TimeConversion.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AIA_DONKI_DMLab/TimeConversion.ipynb
+curl -o /shared/examples/AIA_HMI_DONKI_DMLab/TimeZones.ipynb https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/examples/AIA_DONKI_DMLab/TimeZones.ipynb
 
 
 #Pip Packages and Dependencies
-curl -o python_libraries_dependencies/environment.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/environment.yml
-curl -o python_libraries_dependencies/requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/requirements.txt
-curl -o python_libraries_dependencies/ml.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/ml.yml
-curl -o python_libraries_dependencies/custom_requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/libraries_dependencies/custom_requirements.txt
+curl -o python_libraries_dependencies/environment.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/libraries_dependencies/environment.yml
+curl -o python_libraries_dependencies/requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/libraries_dependencies/requirements.txt
+curl -o python_libraries_dependencies/ml.yml https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/libraries_dependencies/ml.yml
+curl -o python_libraries_dependencies/custom_requirements.txt https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/libraries_dependencies/custom_requirements.txt
 
 #Custom Modules
-curl -o custom_modules/aiaImages.py https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/custom_modules/aiaImages.py
-curl -o custom_modules/hmiImages.py https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/custom_modules/hmiImages.py
-curl -o custom_modules/dmLab.py https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/custom_modules/dmLab.py
-curl -o custom_modules/donkiData.py https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/custom_modules/donkiData.py
+curl -o custom_modules/aiaImages.py https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/custom_modules/aiaImages.py
+curl -o custom_modules/hmiImages.py https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/custom_modules/hmiImages.py
+curl -o custom_modules/dmLab.py https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/custom_modules/dmLab.py
+curl -o custom_modules/donkiData.py https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/custom_modules/donkiData.py
 
-sudo curl -o /etc/profile.d/link_directories.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-lite/main/START_HERE/link_directories.sh
+sudo curl -o /etc/profile.d/link_directories.sh https://raw.githubusercontent.com/indiajacksonphd/Helio-Lite/main/START_HERE/link_directories.sh
 sudo chmod +x /etc/profile.d/link_directories.sh
